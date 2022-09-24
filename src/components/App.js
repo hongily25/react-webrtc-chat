a87baad6492b69c63e18358962713e6ddab47f72
Emily Lam <hlam93@gmail.com>
2022-09-24 17:13:27 -0500

    Add filter

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,24 @@ function App() {
   const [room, setRoom] = useState(null);
   const [username, setUsername] = useState(null);
   const { stream, error } = useUserMediaFromContext();
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    // 👇️ toggle isActive state on click
+    event.preventDefault();
+    setIsActive(current => !current);
+  };
 
   const handleJoin = values => {
-    setRoom(values.room);
-    setUsername(values.username);
+    // setRoom(values.room);
+    // setUsername(values.username);
   };
 
   return (
     <div className="container-fluid">
       {room && username ? (
-        <Room name={room} username={username} stream={stream} />
+        <div></div>
+        // <Room name={room} username={username} stream={stream} />
       ) : (
         <Fragment>
           {error && (
@@ -35,11 +43,23 @@ function App() {
           <div className="row justify-content-center mt-2">
             <UserMediaActions stream={stream} />
           </div>
-          {stream && (
-            <div className="row justify-content-center mt-2">
-              <Video stream={stream} autoPlay muted />
+           (
+            <div>
+              <form className="form-row text-center justify-content-center">
+                <div className="form-group justify-content-center col-md-2">
+                  <button type="submit" className="btn btn-primary" onClick={handleClick}>
+                    {isActive ? 'De-anonymize' : 'Anonymize'}
+                  </button>
+                </div>    
+              </form> 
+              <div className="row justify-content-center mt-2 video">
+                <div id="hide" className={isActive ? 'show-overlay' : ''}></div>
+                <span class="dot" className={isActive ? 'show-dot' : ''}></span>
+                <span class="gradient" className={isActive ? 'show-overlay' : ''}></span>
+                <Video stream={stream} autoPlay muted />
+              </div>
             </div>
-          )}
+          )
         </Fragment>
       )}
     </div>
--- src/components/RoomForm.js
+++ src/components/RoomForm.js
@@ -3,60 +3,38 @@ import PropTypes from "prop-types";
 
 function RoomForm({ onJoin, ...props }) {
   const roomInput = useRef(null);
-  const userNameInput = useRef(null);
+  // const userNameInput = useRef(null);
 
-  useEffect(() => {
-    if (roomInput.current) {
-      roomInput.current.focus();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (roomInput.current) {
+  //     roomInput.current.focus();
+  //   }
+  // }, []);
 
   const handleFormSubmit = event => {
-    event.preventDefault();
+    // event.preventDefault();
+    const el = document.getElementById('hide');
+    console.log({ el });
 
-    const room = roomInput.current.value;
-    const username = userNameInput.current.value;
-    if (room.length === 0 && username.length === 0) {
-      return;
-    }
+    // const room = roomInput.current.value;
+    // const username = userNameInput.current.value;
+    // if (room.length === 0 && username.length === 0) {
+    //   return;
+    // }
 
-    onJoin({ room, username });
+    // onJoin({ room, username });
 
-    roomInput.current.value = "";
-    userNameInput.current.value = "";
+    // roomInput.current.value = "";
+    // userNameInput.current.value = "";
   };
 
   return (
-    <form {...props} onSubmit={handleFormSubmit}>
-      <div className="form-row text-center">
-        <div className="form-group col-md-5">
-          <input
-            ref={roomInput}
-            type="text"
-            className="form-control"
-            placeholder="Type a room name..."
-          />
-        </div>
-        <div className="form-group col-md-5">
-          <input
-            ref={userNameInput}
-            type="text"
-            className="form-control"
-            placeholder="Username"
-          />
-        </div>
-        <div className="form-group col-md-2">
-          <button type="submit" className="btn btn-primary">
-            Join
-          </button>
-        </div>
-      </div>
-    </form>
+<div></div>
   );
 }
 
 RoomForm.propTypes = {
-  onJoin: PropTypes.func.isRequired
+  onJoin: ''
 };
 
 export default RoomForm;
